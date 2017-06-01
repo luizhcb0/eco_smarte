e9a6f34c314c8edad0543dde2eee1feae4c167b3
luizhcb0 <luizhcb0@gmail.com>
2017-05-31 23:32:03 -0300

    Created: Carousel

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,7 @@
 class StaticController < ApplicationController
+  
+  def index
+    @images = ["eco_0.jpg", "eco_1.jpg", "eco_2.jpg", "eco_3.jpg", "eco_4.jpg", "eco_5.jpg"]
+    @describe_text = Text.find_by(id: 1)
+  end
 end
