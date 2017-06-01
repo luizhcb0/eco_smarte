64030b5dab49de58764e7321102e5d99cd3e7243
luizhcb0 <luizhcb0@gmail.com>
2017-05-31 23:46:50 -0300

    Created: FAQ action

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,4 +4,8 @@ class StaticController < ApplicationController
     @images = ["eco_0.jpg", "eco_1.jpg", "eco_2.jpg", "eco_3.jpg", "eco_4.jpg", "eco_5.jpg"]
     @describe_text = Text.find_by(id: 1)
   end
+  
+  def faq
+
+  end
 end
